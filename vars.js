cae6f5fa92d23180ee1a269246843263ed6717ea
tw1ttt3r <tw1ttt3r@gmail.com>
2021-08-25 22:20:33 -0500

    added global variables

---
+++ vars.js
@@ -0,0 +1,12 @@
+const agenda = [];
+const menu = `Bienvenido a su agenda telefónica.
+Total de contactos: ${agenda.length}
+
+    1. Agregar contacto
+    2. Buscar contacto
+    3. Editar contacto
+    4. Eliminar contacto
+    5. Listar contactos
+    6. Salir
+
+`;
